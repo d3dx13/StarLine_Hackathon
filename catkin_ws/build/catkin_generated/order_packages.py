94e2beff4c42afe6a09a53088f57817e5347dd21
d3dx13 <zhidkov2846@gmail.com>
2019-11-17 12:12:19 +0300

    add catkin_ws

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/d3dx13/StarLine_Hackathon/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/d3dx13/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/d3dx13/catkin_ws/devel;/opt/ros/kinetic" != "" else []
